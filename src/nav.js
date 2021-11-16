f2655a50799ae171715704d7f88f216a44075c1b
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-16 01:28:15 -0800

    routing and navbar completed

--- src/nav.js
+++ src/nav.js
@@ -1,10 +1,17 @@
 
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function nav() {
     return (
-        <div>
-            im am navigation
+        <div className="navbar">
+            <div className="nav-left">
+                <img className="nav-logo" src="/react-logo.svg" alt="logo" />
+            </div>
+            <div className="nav-right">
+                <Link to="/" className="nav-links">Home</Link>
+                <Link to="/About" className="nav-links">About</Link>
+            </div>
         </div>
     )
 }
