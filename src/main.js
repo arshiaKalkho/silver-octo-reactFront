8b164f2fec7ac655c07334a3cd7c7dfa8dcd2a9d
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-18 21:57:07 -0800

    added main section to index page

--- src/main.js
+++ src/main.js
@@ -9,13 +9,27 @@ export default class main extends Component {
     render() {
         return (
             <>
-                <div className="main" style={{backgroundImage:"url('/buy-sell-background.jpg')"}}>
-                    
+                <div className="main" style={{backgroundImage:"url('/main-top-background.jpg')"}}>
+                
+                    <div className="main-invite">
+                        <h1>Buy and sell </h1>
+                        <p>Buy and sell your product on our platform for free, Let us worry about shipping, hadeling, payment and even wearhousing if neccesary.</p>
+                    </div>
+                    <div className="main-shipping-image" style={{backgroundImage:"url('/shipping.jpg')"}}>
+                        <p>take a break, make it easier on yourself.</p>
+                    </div>
                 </div>
                 <div className="main-body">
-                    <div className="summery-cards"></div>
-                    <div className="summery-cards"></div>
-                    <div className="summery-cards"></div>
+                    <div className="summery-cards">
+                        <h1>1</h1>
+                        
+                    </div>
+                    <div className="summery-cards">
+                        <h1>2</h1>
+                    </div>
+                    <div className="summery-cards">
+                        <h1>3</h1>
+                    </div>
                 </div>
             </>
         )
