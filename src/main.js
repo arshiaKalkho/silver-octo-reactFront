a7aa2cf5c5dc6006fcc491353f5af852b6728f04
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-18 23:12:11 -0800

    main widget completed with support for small displays

--- src/main.js
+++ src/main.js
@@ -11,13 +11,44 @@ export default class main extends Component {
             <>
                 <div className="main" style={{backgroundImage:"url('/main-top-background.jpg')"}}>
                 
-                    <div className="main-invite">
+                    <div className="main-welcome">
                         <h1>Buy and sell </h1>
                         <p>Buy and sell your product on our platform for free, Let us worry about shipping, hadeling, payment and even wearhousing if neccesary.</p>
                     </div>
-                    <div className="main-shipping-image" style={{backgroundImage:"url('/shipping.jpg')"}}>
-                        <p>take a break, make it easier on yourself.</p>
+                    <div className="main-widget" >
+                        <div className="main-widget-layer" >
+                            <div className="main-widget-layer-inner">
+                                <h3>we ship</h3>
+                                <p>our premium member ship gives you access to unlimited shipments in the continental US</p>
+                            </div>
+                            <div className="main-widget-layer-inner"style={{backgroundImage:"url('/shipping.jpg')"}}>
+
+                            </div>
+                        </div>
+                        <div className="main-widget-layer">
+                            <div className="main-widget-layer-inner" style={{backgroundImage:"url('/payment.jpg')"}}></div>
+                            <div className="main-widget-layer-inner">
+                                <h3>handle tranactions</h3>
+                                <p>we handle transaction, weater using shopify, paypal or visa.</p>
+                            </div>
+
+                        </div>
+                        <div className="main-widget-layer">
+                            <div className="main-widget-layer-inner"><h3>and store</h3>
+                                <p>Don't have storage, As long as your a member you have access to our wearhouses to store your product.</p>
+                                </div>
+                            <div className="main-widget-layer-inner" style={{backgroundImage:"url('/wearhouse.jpg')"}}>
+                            </div>
+                            
+                        </div>
                     </div>
+                
+                
+                
+                
+                
+                
+                
                 </div>
                 <div className="main-body">
                     <div className="summery-cards">
