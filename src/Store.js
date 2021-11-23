f42494de6c290e9de1f6ab144a35ee7586d0e8c0
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-22 20:42:44 -0800

    added store header component

--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import StoreHeader from './StoreHeader'
 
 function Store() {
     return (
-        <div>
-            contact me
+        <div className="store">
+            <StoreHeader/>
+                <div className="sidebar-product-container">
+                    <div className="store-sidebar">
+                        Sidebar
+                    </div>
+                    <div className="products">
+                        products    
+                    </div>                
+            </div>
         </div>
+
     )
 }
 
