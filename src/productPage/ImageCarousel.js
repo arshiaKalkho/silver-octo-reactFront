3daee5ee189467a14adfa9842c2181169a9adc70
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-12-15 06:01:45 -0800

    added working carousel for the product imageCarousel component

--- src/productPage/ImageCarousel.js
+++ src/productPage/ImageCarousel.js
@@ -4,18 +4,30 @@ import React,{useState} from 'react'
 function ImageCarousel(props) {
 
 
-    //const [currImageIndex, setCurrImageIndex] = useState(0)
+    var [currImageIndex, setCurrImageIndex] = useState(0)
     
+    const nextbutton = ()=>{
+        if(JSON.parse(props.image)[currImageIndex+1]){
+            setCurrImageIndex(currImageIndex+1)
+        }
+    }
+    const prevbutton = ()=>{
+        if(currImageIndex > 0){
+            setCurrImageIndex(currImageIndex-1)
+        }
+    }
+
     if(props.image){
         return (
-            <div className="product-page-image" style={{ backgroundImage: `url(${JSON.parse(props.image)[0]})`}}>
-                
+            <div className="product-page-image" style={{ backgroundImage: `url(${JSON.parse(props.image)[currImageIndex]})`}}>
+                <button className="product-page-btn" onClick={()=>prevbutton()}>prev</button>
+                <button className="product-page-btn" onClick={()=>nextbutton()}>next</button>
             </div>
         )
-        }else{
-            return (
-                <div className="product-page-image" style={{textAlign: 'center', marginTop: "8rem"}}> Loading...</div>
-            )
-        }
+    }else{
+        return (
+            <div className="product-page-image" style={{textAlign: 'center', marginTop: "8rem"}}> Loading...</div>
+        )
+    }
 }
 export default ImageCarousel
