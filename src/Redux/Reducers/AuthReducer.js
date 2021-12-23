83f3db6522547f8c66f292c13c311d83a0f8834b
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-12-23 13:27:36 -0800

    added auth components

---
+++ src/Redux/Reducers/AuthReducer.js
@@ -0,0 +1,15 @@
+const authState = {//the initial state of the reducer
+  isLoggedIn: false,
+  user:{
+    name:"",
+    expires_at: "",
+    jwttoken:"",
+    authorities:[""]
+  }
+}
+
+
+const authReducer = (state = authState, action)=>{
+  return state
+}
+export default authReducer;
